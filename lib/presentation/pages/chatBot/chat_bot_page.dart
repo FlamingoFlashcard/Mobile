b9d4f8172f688ad1d4e5ba18f8e57f8a599604f6
QUOC HAI <haiquocntp@gmail.com>
2025-05-07 15:22:39 +0700

    feat: Add template for chatbot screen

--- lib/presentation/pages/chatBot/chat_bot_page.dart
+++ lib/presentation/pages/chatBot/chat_bot_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:lacquer/config/theme.dart';
 
 class ChatBotPage extends StatelessWidget {
   const ChatBotPage({super.key});
@@ -7,18 +9,53 @@ class ChatBotPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Chat bot Screen')),
-
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            ScaffoldMessenger.of(
-              context,
-            ).showSnackBar(const SnackBar(content: Text('Button Pressed!')));
-          },
-
-          child: const Text('Press me'),
+      backgroundColor: CustomTheme.white,
+      body: Container(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Discover culture, cuisine and more with',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 30, 
+                fontWeight: FontWeight.bold,
+                fontFamily: GoogleFonts.lora().fontFamily,
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Mr. Calligraphy',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 35, 
+                    fontWeight: FontWeight.bold,
+                    fontFamily: GoogleFonts.moonDance().fontFamily,
+                  ),
+                ),
+                const SizedBox(width: 5),
+                Image(
+                  image: AssetImage('assets/images/inkwell.png'),
+                  width: 35,
+                  height: 35,
+                ),
+              ],
+            ),
+            const SizedBox(height: 20),
+            SearchBar(
+              hintText: 'Ask anything',
+              onChanged: (value) {
+                // Handle search input
+              },
+            )
+          ],
         ),
       ),
     );
   }
+
+  
 }
